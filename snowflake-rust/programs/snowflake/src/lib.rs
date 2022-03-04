21028bc7493f6e09c09bc50eee222635e58759c4
nguyenvminh <31537663+nguyenvminh@users.noreply.github.com>
2022-03-04 19:52:07 +1100

    SF-87: only assigned operator can mark timed flow as error (#47)

--- snowflake-rust/programs/snowflake/src/lib.rs
+++ snowflake-rust/programs/snowflake/src/lib.rs
@@ -94,8 +94,12 @@ pub mod snowflake {
         let pda_bump = *ctx.bumps.get("pda").unwrap();
         charge_fee(&ctx, pda_bump)?;
 
+        let operator = &ctx.accounts.caller;
+        let program_settings = &ctx.accounts.program_settings;
         let flow = &mut ctx.accounts.flow;
         let now = Clock::get()?.unix_timestamp;
+        
+        require!(program_settings.can_operator_excecute_flow(now, &flow.key(), operator.key), ErrorCode::JobIsNotAssignedToOperator);
         
         require!(flow.is_schedule_expired(now), ErrorCode::CannotMarkJobAsErrorIfItsWithinSchedule);
         
